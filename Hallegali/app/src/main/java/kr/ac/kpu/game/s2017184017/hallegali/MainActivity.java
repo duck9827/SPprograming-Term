13cf88d1178370a65f24429b85775a2ce45d0cad
오리9827 <sspure@naver.com>
2021-05-21 16:12:33 +0900

    상단 타이틀바, 상태바 제거

--- Hallegali/app/src/main/java/kr/ac/kpu/game/s2017184017/hallegali/MainActivity.java
+++ Hallegali/app/src/main/java/kr/ac/kpu/game/s2017184017/hallegali/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); //가로모드 고정
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
     }
